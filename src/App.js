41770f73c06a134cfe3a0ed8b0570ebab70c6ec4
Aleemcode <aakinyoola857@gmail.com>
2020-08-30 23:36:08 +0100

    Dark Theme Component created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.scss';
+import SmallCard from './SmallCard';
+
+function App() {
+  return (
+    <div className="hero">
+      <header className="hero-header">
+      <SmallCard />
+      </header>
+    </div>
+  );
+}
+
+export default App;
